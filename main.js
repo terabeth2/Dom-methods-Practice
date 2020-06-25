f7fa23061c6ea9890448221f8474a229814d3211
terabeth2 <terabeth@me.com>
2020-06-24 21:27:44 -0500

    second attempt

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ const button = document.createElement('button')
 const main = document.createElement('main')
 document.body.prepend(main)
 document.body.prepend(button)
-
+button.append('Remove')
 let imgElement = document.createElement('img')
 imgElement.src = 'https://www.cat-breeds-encyclopedia.com/image-files/xtortoiseshell-kitten.jpg.pagespeed.ic.0ICFzJUYJW.jpg'
 main.append(imgElement)
 
 let anchorElement = document.createElement('a')
 anchorElement.href = 'https://www.cat-breeds-encyclopedia.com/image-files/xtortoiseshell-kitten.jpg.pagespeed.ic.0ICFzJUYJW.jpg'
-main.append('THE TORTOISESHELL CAT')
-
-main.addEventListener('click', function () {
-button.remove()
+anchorElement.append('THE TORTOISESHELL CAT')
+main.append(anchorElement)
+button.addEventListener('click', function () {
+main.remove()
 })
